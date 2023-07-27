146230d576ff8c8059c845af7b74912f3b21fd4d
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-27 21:56:37 +0530

    Added basic components to Welcome Page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,6 +5,8 @@ import { BsInfoCircle } from "react-icons/bs";
 import { Loader } from "./";
 
 const Welcome = () => {
+	const commonStyles =
+		"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 	const connectWallet = () => {};
 
 	return (
@@ -26,6 +28,14 @@ const Welcome = () => {
 					>
 						<p className="text-white font-semibold">Connect Wallet</p>
 					</button>
+					<div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+						<div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
+						<div className={`${commonStyles}`}>Security</div>
+						<div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+						<div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
+						<div className={`${commonStyles}`}>Low Fees</div>
+						<div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+					</div>
 				</div>
 			</div>
 		</div>
